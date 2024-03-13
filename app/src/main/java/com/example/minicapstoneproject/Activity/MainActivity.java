5e648a0cda7d9629515a6c4731e9c2856b412633
Chí Hiếu <hieutrinh09112002@gmail.com>
2024-03-13 21:28:15 +0700

    final fix

--- app/src/main/java/com/example/minicapstoneproject/Activity/MainActivity.java
+++ app/src/main/java/com/example/minicapstoneproject/Activity/MainActivity.java
@@ -82,4 +82,5 @@ public class MainActivity extends AppCompatActivity {
         }
         adapter.filterList(filteredList);
     }
+
 }
--- app/src/main/java/com/example/minicapstoneproject/Adapter/ProductAdapter.java
+++ app/src/main/java/com/example/minicapstoneproject/Adapter/ProductAdapter.java
@@ -23,19 +23,22 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     Context context;
     ViewholderPupListBinding binding;
 
-    public ProductAdapter(ArrayList<Product> items){
+    public ProductAdapter(ArrayList<Product> items) {
         this.items = items;
     }
+
     @NonNull
     @Override
     public ProductAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        binding = ViewholderPupListBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        binding = ViewholderPupListBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
         context = parent.getContext();
         return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductAdapter.ViewHolder holder, int position) {
+        ViewholderPupListBinding binding = holder.binding; // Lấy ra binding của ViewHolder hiện tại
+
         binding.titleTxt.setText(items.get(position).getTitle());
         binding.priceTxt.setText(items.get(position).getPrice() + "$");
         binding.scoreTxt.setText(items.get(position).getScore() + "");
@@ -46,7 +49,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
         //Load image to display using Glide framework
         Glide.with(context)
                 .load(drawableResourceID)
-                .transform(new GranularRoundedCorners(30,30,0,30))
+                .transform(new GranularRoundedCorners(30, 30, 0, 30))
                 .into(binding.pic);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -63,14 +66,18 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     public int getItemCount() {
         return items.size();
     }
+
     public void filterList(List<Product> filteredList) {
         items = (ArrayList<Product>) filteredList;
         notifyDataSetChanged();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        public ViewHolder(ViewholderPupListBinding binding){
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ViewholderPupListBinding binding;
+
+        public ViewHolder(ViewholderPupListBinding binding) {
             super(binding.getRoot());
+            this.binding = binding;
         }
     }
 }
